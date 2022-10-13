baceccbde010764e5b4e88a30b09441f89b588d9
Denilson <denilsonrabelo.dev@gmail.com>
2022-10-13 13:34:37 -0300

    removendo rota

--- view/loginRouter.js
+++ view/loginRouter.js
@@ -8,9 +8,7 @@ const router = express.Router()
 router.post("/login", (req, res) => {
     return loginController.login(req, res)
 })
-router.post("/123", (req ,res) => {
-    return  loginController.addlogin(req,res)
-})
+
 
 
 module.exports = app => app.use('/receitas', router)
