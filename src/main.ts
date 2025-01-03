7df7793f9ceb0fbbfcfda95de92fa6530da5bdfa
Denilson Rabelo <denilsonrabelo.dev@gmail.com>
2025-01-03 12:16:33 -0300

    feat: enhance CORS configuration for improved security and flexibility

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(3000);
 }
 bootstrap();
